30f088cd279af6fbe48607cd457e78a8dc269da2
ruslan0310 <96051719+ruslan0310@users.noreply.github.com>
2022-03-12 11:05:20 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+<command-palette-pjax-metadata hidden="" class="js-command-palette-pjax-metadata" data-pjax-replace="" id="command-palette-pjax-metadata" data-default-scope-id="U_kgDOBbmiBw" data-default-scope-type="User" data-catalyst="">
+</command-palette-pjax-metadata>
